7494e863c01f7b71e900b0cfdd0316c5991ff16b
aiyaya2023 <133057442+aiyaya2023@users.noreply.github.com>
2023-06-02 15:09:49 -0700

    z4j9wfsss
    z4j9wf22

--- testcode/browser/cypress.config.js
+++ testcode/browser/cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'ymhxfj',
+  projectId: "z4j9wf",
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {
