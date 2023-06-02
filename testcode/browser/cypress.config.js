b5aeaee4b339b6cb8de06dde89ced06ae437a1d4
aiyaya2023 <133057442+aiyaya2023@users.noreply.github.com>
2023-06-02 15:34:07 -0700

    test
    sss

--- testcode/browser/cypress.config.js
+++ testcode/browser/cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: "z4j9wf",
+  "env": {
+    "CYPRESS_RECORD_KEY": "cbfa8b78-74fa-44d6-99cb-aa18e0044dc4"
+  },
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {
