93c8b2dc96808202285eff977faa513694d7489c
aiyaya2023 <133057442+aiyaya2023@users.noreply.github.com>
2023-06-02 15:34:48 -0700

    ttt
    sss

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "z4j9wf",
+  "env": {
+    "CYPRESS_RECORD_KEY": "cbfa8b78-74fa-44d6-99cb-aa18e0044dc4"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
