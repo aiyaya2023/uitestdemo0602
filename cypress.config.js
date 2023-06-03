461ef31559a4ca45be15b53988ea392932aa69d0
aiyaya2023 <133057442+aiyaya2023@users.noreply.github.com>
2023-06-02 17:31:17 -0700

    Update cypress.config.js
    sss

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "z4j9wf",
+  key: "cbfa8b78-74fa-44d6-99cb-aa18e0044dc4",
   "env": {
     "CYPRESS_RECORD_KEY": "cbfa8b78-74fa-44d6-99cb-aa18e0044dc4"
   },
